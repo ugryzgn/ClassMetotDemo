1e0b1476d616fd09ce9d155b759b25f66c0e0f8c
ugryzgn <ugryazgn@gmail.com>
2021-01-20 03:36:34 +0300

    Update MusteriManager.cs

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,32 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
+
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            Console.WriteLine("Hoşgeldiniz!" + "  " + musteri.Id + " - " + musteri.Ad + " " + musteri.Soyad +" - "+ musteri.MevduatMiktarı + " $");
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        }
+
+        public void Listele(Musteri musteri) 
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------- Müşteri Listesi -------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine(musteri.Id + " - " + musteri.Ad + " " + musteri.Soyad + " --> " + musteri.MevduatMiktarı + " $");
+            
+        }
+
+        public void Sil(Musteri musteri) 
+        {
+
+            Console.WriteLine(" ");
+            Console.WriteLine("##########################################################");
+            Console.WriteLine("Yine bekleriz... " + musteri.Id + " - " + musteri.Ad + " " + musteri.Soyad + " - " + musteri.MevduatMiktarı + " $");
+            Console.WriteLine("##########################################################");
+        }   
     }
 }
