bac23ba69cadabc7608d907bbbdd37067da7db10
ugryzgn <ugryazgn@gmail.com>
2021-01-20 03:49:57 +0300

    Update MusteriManager.cs

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -11,15 +11,15 @@ namespace ClassMetotDemo
         public void Ekle(Musteri musteri)
         {
             Console.WriteLine(" ");
-            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<");
             Console.WriteLine("Hoşgeldiniz!" + "  " + musteri.Id + " - " + musteri.Ad + " " + musteri.Soyad +" - "+ musteri.MevduatMiktarı + " $");
-            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>> o <<<<<<<<<<<<<<<<<<<<<<<<<<<<");
         }
 
         public void Listele(Musteri musteri) 
         {
             Console.WriteLine(" ");
-            Console.WriteLine("------------------- Müşteri Listesi -------------------");
+            Console.WriteLine("-------------------  Müşteri  Listesi  -------------------");
             Console.WriteLine(" ");
             Console.WriteLine(musteri.Id + " - " + musteri.Ad + " " + musteri.Soyad + " --> " + musteri.MevduatMiktarı + " $");
             
